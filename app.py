c44848f2a2dc152072599e3c599c5d4d8da12c67
oliviawissig <oliviawissig@gmail.com>
2024-03-26 12:59:47 -0700

    added functionality to speak what is entered into the text box

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
 import os
+import time
 from dotenv import load_dotenv
 from deepgram import (
     DeepgramClient,
-    PrerecordedOptions,
-    FileSource,
+    SpeakOptions,
 )
-# from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pydub import AudioSegment
+from pydub.playback import play
+
+app = Flask(__name__, template_folder="templates")
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 load_dotenv()
-AUDIO_FILE = "no_sleep_podcast.mp3"
-DEEPGRAM_API_KEY = os.getenv('DEEPGRAM_API_KEY')
+SPEAK_OPTIONS = {"text": "Hello!"}
+DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
 
-# app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# @app.route("/")
-# def hello_world():
-#     return render_template('index.html')
+@app.route("/speak", methods=["POST"])
+def speak():
+    audio_str = request.form['content']
+    SPEAK_OPTIONS = {"text":audio_str}
 
-def main():
     try:
         deepgram = DeepgramClient(DEEPGRAM_API_KEY)
-
-        with open(AUDIO_FILE, "rb") as file:
-            buffer_data = file.read()
-
-        payload: FileSource = {
-            "buffer": buffer_data,
-        }
-
-        options = PrerecordedOptions(model="nova-2", smart_format=True)
-        response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
+        options = SpeakOptions(
+            model="aura-luna-en", encoding="linear16", container="wav"
+        )
+        response = deepgram.speak.v("1").save(
+            STATIC_DIR + "/speakup_output.wav", SPEAK_OPTIONS, options
+        )
         print(response.to_json(indent=4))
-
     except Exception as e:
         print(f"Exception: {e}")
 
-if __name__ == '__main__':
-	main()
+    song = AudioSegment.from_wav(STATIC_DIR + "/speakup_output.wav")
+    play(song)
+
+    return render_template("audio.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
